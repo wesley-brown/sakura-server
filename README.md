4164a73f956f3fd86830b91e3b6100eb6b40f84d
Wesley Brown <wesleybrown@wesleybrown.net>
2022-11-20 13:36:22 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Sakura Server
+The game server for my "Sakura" game.
+
+## Build
+To build all the projects, run the following command:
+```shell
+dotnet build
+```
+
+## Run
+To run the Sakura server, run the following command:
+```shell
+dotnet run --project src/Sakura.Server
+```
---
+++ src/Sakura.Server/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Starting Sakura server...");
+
+var lastInSeconds = HighResolutionTimeInSeconds();
+var tickLengthInSeconds = 1d / 60.0d;
+var accumulator = 0d;
+
+while (true)
+{
+    var nowInSeconds = HighResolutionTimeInSeconds();
+    var elapsedSeconds = nowInSeconds - lastInSeconds;
+    lastInSeconds = nowInSeconds;
+    accumulator += elapsedSeconds;
+
+    while (accumulator >= tickLengthInSeconds)
+    {
+        TickSimulation();
+        accumulator -= tickLengthInSeconds;
+    }
+}
+
+void TickSimulation()
+{
+}
+
+double HighResolutionTimeInSeconds()
+{
+    long nowInDotNetTicks = DateTime.UtcNow.Ticks;
+    // One .NET tick is 100 nanoseconds
+    long nowInNanoseconds = nowInDotNetTicks * 100;
+    double elapsedSeconds = (double)(nowInNanoseconds / 1000000000d);
+    return elapsedSeconds;
+}
